ea89a6b929bce7be8ef6b77baff463b968396af1
Kamlesh Kumar <jakewhitman001@gmail.com>
2022-02-07 17:07:46 +0530

    Added Question 1

---
+++ Java/Question1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String[] args) {
+        //Question. Java Stdin and Stdout I
+        Scanner sc = new Scanner(System.in);
+        //solution1
+        int a, b, c;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
